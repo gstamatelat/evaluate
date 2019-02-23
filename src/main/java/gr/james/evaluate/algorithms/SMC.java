5cdf0eb717f46139898f7b95865ed0ec5fb7ae68
Giorgos Stamatelatos <gstamatelat@gmail.com>
2019-02-23 20:08:41 +0200

    Implement SMC

--- src/main/java/gr/james/evaluate/algorithms/SMC.java
+++ src/main/java/gr/james/evaluate/algorithms/SMC.java
@@ -3,6 +3,9 @@ package gr.james.evaluate.algorithms;
 import gr.james.evaluate.ds.Partition;
 import gr.james.evaluate.ds.Result;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Simple Matching coefficient implementation.
  */
@@ -31,8 +34,44 @@ public class SMC {
         }
     }
 
+    /**
+     * Returns the Simple Matching coefficient of two partitions.
+     * <p>
+     * This method is commutative (given no exception):
+     * <pre><code>
+     * assert smc(a, b) == smc(b, a);
+     * </code></pre>
+     *
+     * @param a   one partition
+     * @param b   the other partition
+     * @param <T> the type of elements
+     * @return the Simple Matching coefficient between {@code a} and {@code b}
+     * @throws NullPointerException     if {@code a} or {@code b} is {@code null}
+     * @throws IllegalArgumentException if {@code a} and {@code b} do not contain exactly the same elements
+     */
     public static <T> double smc(Partition<T> a, Partition<T> b) {
-        // TODO
-        return 0;
+        if (!a.elements().equals(b.elements())) {
+            throw new IllegalArgumentException("a and b must have the same elements");
+        }
+
+        long common = 0;
+        long all = 0;
+
+        final List<T> vertices = new ArrayList<>(a.elements());
+        for (int i = 0; i < vertices.size() - 1; i++) {
+            for (int j = i + 1; j < vertices.size(); j++) {
+                final boolean aHas = a.connected(vertices.get(i), vertices.get(j));
+                final boolean bHas = b.connected(vertices.get(i), vertices.get(j));
+                if (aHas && bHas) {
+                    common++;
+                }
+                if (!aHas && !bHas) {
+                    common++;
+                }
+                all++;
+            }
+        }
+
+        return (double) common / (double) all;
     }
 }
