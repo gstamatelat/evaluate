c4305da7b0e3f0ad64f5af6fd1551f1de31a6c66
Giorgos Stamatelatos <gstamatelat@gmail.com>
2019-02-23 19:58:04 +0200

    Implement Jaccard on partitions

--- src/main/java/gr/james/evaluate/algorithms/Jaccard.java
+++ src/main/java/gr/james/evaluate/algorithms/Jaccard.java
@@ -3,6 +3,9 @@ package gr.james.evaluate.algorithms;
 import gr.james.evaluate.ds.Partition;
 import gr.james.evaluate.ds.Result;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Jaccard index implementation.
  */
@@ -31,8 +34,43 @@ public class Jaccard {
         }
     }
 
+    /**
+     * Returns the Jaccard index of two partitions.
+     * <p>
+     * This method is commutative (given no exception):
+     * <pre><code>
+     * assert jaccard(a, b) == jaccard(b, a);
+     * </code></pre>
+     *
+     * @param a   one partition
+     * @param b   the other partition
+     * @param <T> the type of elements
+     * @return the Jaccard index between {@code a} and {@code b}
+     * @throws NullPointerException     if {@code a} or {@code b} is {@code null}
+     * @throws IllegalArgumentException if {@code a} and {@code b} do not contain exactly the same elements
+     */
     public static <T> double jaccard(Partition<T> a, Partition<T> b) {
-        // TODO
-        return 0;
+        if (!a.elements().equals(b.elements())) {
+            throw new IllegalArgumentException("a and b must have the same elements");
+        }
+
+        long intersection = 0;
+        long union = 0;
+
+        final List<T> vertices = new ArrayList<>(a.elements());
+        for (int i = 0; i < vertices.size() - 1; i++) {
+            for (int j = i + 1; j < vertices.size(); j++) {
+                final boolean aHas = a.connected(vertices.get(i), vertices.get(j));
+                final boolean bHas = b.connected(vertices.get(i), vertices.get(j));
+                if (aHas && bHas) {
+                    intersection++;
+                }
+                if (aHas || bHas) {
+                    union++;
+                }
+            }
+        }
+
+        return (double) intersection / (double) union;
     }
 }
