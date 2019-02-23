8e59cae327b4396780433a7863b75aae555577ad
Giorgos Stamatelatos <gstamatelat@gmail.com>
2019-02-24 00:05:50 +0200

    Implement Cosine for partitions

--- src/main/java/gr/james/evaluate/algorithms/Cosine.java
+++ src/main/java/gr/james/evaluate/algorithms/Cosine.java
@@ -4,8 +4,11 @@ import gr.james.evaluate.ds.Partition;
 import gr.james.evaluate.ds.Result;
 import gr.james.evaluate.ds.ValueList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Cosine similarity coefficient implementation.
+ * Cosine similarity implementation.
  */
 public final class Cosine {
     /**
@@ -67,8 +70,47 @@ public final class Cosine {
         return numerator / (Math.sqrt(denominatorA) * Math.sqrt(denominatorB));
     }
 
+    /**
+     * Returns the Cosine similarity of two partitions.
+     * <p>
+     * This method is commutative (given no exception):
+     * <pre><code>
+     * assert cosine(a, b) == cosine(b, a);
+     * </code></pre>
+     *
+     * @param a   one partition
+     * @param b   the other partition
+     * @param <T> the type of elements
+     * @return the Cosine similarity between {@code a} and {@code b}
+     * @throws NullPointerException     if {@code a} or {@code b} is {@code null}
+     * @throws IllegalArgumentException if {@code a} and {@code b} do not contain exactly the same elements
+     */
     public static <T> double cosine(Partition<T> a, Partition<T> b) {
-        // TODO
-        return 0;
+        if (!a.elements().equals(b.elements())) {
+            throw new IllegalArgumentException("a and b must have the same elements");
+        }
+
+        long intersection = 0;
+        long cardinality1 = 0;
+        long cardinality2 = 0;
+
+        final List<T> vertices = new ArrayList<>(a.elements());
+        for (int i = 0; i < vertices.size() - 1; i++) {
+            for (int j = i + 1; j < vertices.size(); j++) {
+                final boolean aHas = a.connected(vertices.get(i), vertices.get(j));
+                final boolean bHas = b.connected(vertices.get(i), vertices.get(j));
+                if (aHas && bHas) {
+                    intersection++;
+                }
+                if (aHas) {
+                    cardinality1++;
+                }
+                if (bHas) {
+                    cardinality2++;
+                }
+            }
+        }
+
+        return (double) intersection / Math.sqrt(1.0 * cardinality1 * cardinality2);
     }
 }
